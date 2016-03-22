94a055f1eaed684c310dc6af6bc15c18d77cba98
Jørgen Høgberget <jorgen_da_mad@hotmail.com>
2016-03-23 00:24:31 +0100

    Reworked job distribution system to full master/slave model. A job fetches a new job from the master when it is complete instead of every cpu getting a fixed share of the total jobs. Also implemented support for linked parameters, such that several parameters can be changed at once without counting all combinations of them. General cleanups and improvements to code structure and tests.

--- ParameterJuggler.py
+++ ParameterJuggler.py
@@ -17,11 +17,10 @@ except ImportError:
 
 class Worker(threading.Thread):
 
-    def __init__(self, controller, exec_program_rule, chunk, proc, *args, **kwargs):
+    def __init__(self, controller, exec_program_rule, proc, *args, **kwargs):
 
         self.controller = controller
         self.exec_program_rule = exec_program_rule
-        self.chunk = chunk
         self.proc = proc
 
         self.args = args
@@ -33,7 +32,16 @@ class Worker(threading.Thread):
         self.controller.stopped = True
 
     def run(self):
-        self.controller.run_chunk(self.exec_program_rule, self.chunk, self.proc, *self.args, **self.kwargs)
+        while not len(self.controller.combinations) == 0:
+            parameters = self.controller.get_parameters_thread()
+            success = self.controller.run_parameters(self.exec_program_rule,
+                                                     parameters,
+                                                     self.proc,
+                                                     *self.args,
+                                                     **self.kwargs)
+
+            if success != 0 and not self.stopped:
+                self.controller.stop(self.proc, success)
 
 
 class ParameterSet:
@@ -43,15 +51,22 @@ class ParameterSet:
                  variable_pattern_in_config,
                  regex_flags=[]):
 
+        if type(variable_pattern_in_config) is not list:
+            variable_pattern_in_config = [variable_pattern_in_config]
+
         with open(config_filename, "r") as f:
 
-            match = findall(variable_pattern_in_config, f.read(), *regex_flags)
+            txt = f.read()
+            for pattern in variable_pattern_in_config:
+                match = findall(pattern, txt, *regex_flags)
 
-        if not match:
-            raise RuntimeError("Pattern '%s' does not match anything in config file '%s'" % (variable_pattern_in_config, config_filename))
+            if not match:
+                raise RuntimeError("Pattern '%s' does not match anything in config file '%s'" % (pattern,
+                                                                                                 config_filename))
 
         self.config_filename = config_filename
         self.variable_pattern_in_config = variable_pattern_in_config
+        self.set = None
 
         self.regex_flags = regex_flags
 
@@ -62,25 +77,15 @@ class ParameterSet:
 
     def initialize_set(self, set):
 
-        self.set = set
+        if len(self.variable_pattern_in_config) != 1:
+            if len(set) != len(self.variable_pattern_in_config):
+                print len(set), len(self.variable_pattern_in_config)
+                raise RuntimeError("Set length must match the number of patterns.")
 
-    def initialize_set_incr(self,
-                            start,
-                            stop,
-                            increment,
-                            increment_rule=lambda previous, increment: previous + increment):
+            self.set = zip(*set)
 
-        set = []
-
-        value = start
-
-        while value <= stop:
-
-            set.append(value)
-
-            value = increment_rule(value, increment)
-
-        self.initialize_set(set)
+        else:
+            self.set = [tuple([x]) for x in set]
 
     @staticmethod
     def invert_regex(pattern):
@@ -132,24 +137,24 @@ class ParameterSet:
 
         controller.register_config_file(config_out)
 
-    def write_value(self, value, proc):
+    def write_value(self, values, proc):
 
         config_out = self.get_config_name(proc)
 
         with open(config_out, 'r') as config_file:
 
-            prev_config_raw_text = config_file.read()
+            config_raw_text = config_file.read()
 
-        new_config_raw_text = sub(self.invert_regex(self.variable_pattern_in_config),
+
+        for i, value in enumerate(values):
+            pattern = self.invert_regex(self.variable_pattern_in_config[i])
+            config_raw_text = sub(pattern,
                                   r"%s%s%s" % (r"\g<1>", value, r"\g<2>"),
-                                  prev_config_raw_text,
+                                  config_raw_text,
                                   *self.regex_flags)
 
         with open(config_out, 'w') as config_file:
-            config_file.write(new_config_raw_text)
-
-
-
+            config_file.write(config_raw_text)
 
 
 class ParameterSetController:
@@ -159,6 +164,10 @@ class ParameterSetController:
         self.copied_config_files = []
 
         self.all_threads = []
+        self.parameter_lock = threading.Lock()
+
+        self.shuffle = False
+
         self.stopped = False
 
         self.use_mpi = use_mpi
@@ -195,15 +204,16 @@ class ParameterSetController:
             os.remove(config_name)
         self.copied_config_files = []
 
-    def get_rank(self):
-        if self.use_mpi:
+    @staticmethod
+    def get_rank(use_mpi):
+        if use_mpi:
             return comm.rank
         else:
             return 0
 
     def n_nodes(self):
         if self.use_mpi:
-            return comm.size
+            return comm.size-1
         else:
             return 1
 
@@ -223,6 +233,10 @@ class ParameterSetController:
                     skip = True
             kwargs.pop("ask")
 
+        if "shuffle" in kwargs:
+            if kwargs.pop("shuffle") is True:
+                self.shuffle = True
+
         n_procs = self.n_nodes()
 
         if "n_procs" in kwargs:
@@ -236,77 +250,30 @@ class ParameterSetController:
             kwargs.pop("n_procs")
 
         if n_procs > n:
-            if self.get_rank() == 0:
-                raise ValueError("Number of processes %d is too high for %d processes." % (n_procs, n))
-            else:
-                return 1
+            if self.get_rank(self.use_mpi) == 0:
+                print "Number of processes %d is too high for %d processes. Truncating..." % (n_procs, n)
+                n_procs = n
 
         if skip:
             ans = ""
         else:
-            ans = raw_input("Press either: \n\tenter to run %d simulations."
-                            "\n\tt [time per sim in seconds] for a time estimate. "
-                            "\n\tq to end.\n" % n)
+            ans = raw_input("Press enter to run %d simulations." % n)
 
-        if ans.startswith("t "):
-            time = float(ans.lstrip("t "))
-            t = n*time/n_procs
-
-            unit = ""
-            if t > 3600:
-                t /= 3600.
-                unit = "hours"
-
-            elif t > 60:
-                t /= 60.
-                unit = "minutes"
-
-            else:
-                unit = "seconds"
-
-            print "Expected time: %g %s" % (t, unit)
-
-            return self.run(execute_program_rule, *args, **kwargs)
-
-        elif ans == "q":
+        if ans != "":
             print "exiting.."
             return 1
 
-        combinations = list(product(*[pset.set for pset in self.parameter_sets]))*self.repeats
+        self.combinations = list(product(*[pset.set for pset in self.parameter_sets]))*self.repeats
 
-        if "shuffle" in kwargs:
-            if kwargs.pop("shuffle") is True:
+        n_per_proc = len(self.combinations)/n_procs
+        remainder = len(self.combinations) - n_per_proc*n_procs
 
-                if self.use_mpi:
-                    random.seed(123456789)
+        if self.get_rank(self.use_mpi) == 0:
+            print "%d*%d + %d processes" % (n_per_proc,
+                                            n_procs,
+                                            remainder)
 
-                random.shuffle(combinations)
-
-        n_per_proc = len(combinations)/n_procs
-
-        if self.get_rank() == 0:
-            print n_per_proc, "processes"
-
-        remainder = len(combinations) - n_per_proc*n_procs
-
-        self.all_threads = []
-        self.stopped = False
-
-        chunks = [None for n in range(n_procs)]
-
-        prev = 0
-        for proc in range(n_procs):
-
-            N = n_per_proc
-
-            if proc < remainder:
-                N += 1
-
-            chunks[proc] = combinations[prev:prev+N]
-
-            prev += N
-
-        self.start_runs(execute_program_rule, chunks, *args, **kwargs)
+        self.start_runs(execute_program_rule, n_procs, *args, **kwargs)
 
         self.clean()
 
@@ -323,23 +290,51 @@ class ParameterSetController:
         for parameter_set in self.parameter_sets:
             parameter_set.copy_config(self, proc)
 
-    def start_runs(self, execute_program_rule, chunks, *args, **kwargs):
+    def start_runs(self, execute_program_rule, n_procs, *args, **kwargs):
 
         if self.use_mpi:
 
-            self.dump_config_files(comm.rank)
+            #master
+            if self.get_rank(self.use_mpi) == 0:
+                n_jobs = len(self.combinations)
 
-            chunk = chunks[comm.rank]
+                #seed slaves
+                for proc in range(1, comm.size):
+                    comm.send(self.get_parameters(), dest=proc, tag=proc)
 
-            self.run_chunk(execute_program_rule, chunk, comm.rank, *args, **kwargs)
+                status = MPI.Status()
+                n = 0
+                while n != n_jobs:
+                    success = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+                    n+=1
+
+                    if success != 0:
+                        self.stop(status.Get_source(), success)
+
+                    comm.send(self.get_parameters(), dest=status.Get_source(), tag=status.Get_tag())
+
+            #slave
+            else:
+                self.dump_config_files(comm.rank)
+
+                parameters = comm.recv(source=0, tag=comm.rank)
+
+                while parameters is not None:
+                    success = self.run_parameters(execute_program_rule, parameters, comm.rank, *args, **kwargs)
+
+                    comm.send(success, dest=0, tag=comm.rank)
+                    parameters = comm.recv(source=0, tag=comm.rank)
 
         else:
 
-            for proc, chunk in enumerate(chunks):
+            self.all_threads = []
+            self.stopped = False
+
+            for proc in range(n_procs):
 
                 self.dump_config_files(proc)
 
-                thread = Worker(self, execute_program_rule, chunk, proc, *args, **kwargs)
+                thread = Worker(self, execute_program_rule, proc, *args, **kwargs)
 
                 self.all_threads.append(thread)
 
@@ -348,19 +343,30 @@ class ParameterSetController:
             for thread in self.all_threads:
                 thread.join()
 
-    def run_chunk(self, execute_program_rule, chunk, proc, *args, **kwargs):
+    def get_parameters(self):
 
-        for combination in chunk:
+        if not self.combinations:
+            return None
 
-            if self.stopped:
-                break
+        if self.shuffle:
+            i = random.randint(0, len(self.combinations) - 1)
+            parameters = self.combinations.pop(i)
+        else:
+            parameters = self.combinations.pop()
 
-            self.write_combination(proc, combination)
+        return parameters
 
-            success = execute_program_rule(proc, combination, *args, **kwargs)
+    def get_parameters_thread(self):
 
-            if success != 0 and not self.stopped:
-                self.stop(proc, success)
+        self.parameter_lock.acquire()
+        parameters = self.get_parameters()
+        self.parameter_lock.release()
+
+        return parameters
+
+    def run_parameters(self, execute_program_rule, parameters, proc, *args, **kwargs):
+            self.write_combination(proc, parameters)
+            return execute_program_rule(proc, parameters, *args, **kwargs)
 
     def write_combination(self, proc, combination):
 
@@ -397,7 +403,9 @@ def quick_replace(cfg, name, value, all_ranks=False):
 
 def exec_test_function(proc, combination):
 
-    time.sleep(proc/10.)
+    combination = tuple([x[0] for x in combination])
+
+    time.sleep(proc/5.*random.random())
 
     testfile_name = "/tmp/test_paramloop_%d.cfg" % proc
 
@@ -415,13 +423,51 @@ def exec_test_function(proc, combination):
             print "combination", combination, "failed on proc %d: %s != %s" % (proc, str(results), str(combination))
             return 1
 
+n_per_proc = []
+
+def exec_test_function2(proc, combination):
+
+    time.sleep(proc/5.*random.random())
+
+    testfile_name = "/tmp/test_paramloop_%d.cfg" % proc
+    testfile2_name = "/tmp/test_paramloop2_%d.cfg" % proc
+
+    with open(testfile_name, 'r') as testfile:
+        testfile_raw_text = testfile.read()
+    with open(testfile2_name, 'r') as testfile:
+        testfile2_raw_text = testfile.read()
+
+    a1 = findall(r"a\s*\=\s*\[\s*(\d+)\s*\,\s*.*\s*\,\s*\d+\s*\]", testfile_raw_text)[0]
+    a2 = findall(r"a\s*\=\s*(\d+)\;", testfile2_raw_text)[0]
+    b2 = findall(r"b\s*\=\s*(\d+)\;", testfile2_raw_text)[0]
+
+    success = (int(a1) == combination[0][0]) and (int(a2) == combination[1][0]) and (int(b2) == combination[1][1])
+
+    n_per_proc[proc] += 1
+
+    if not success:
+        print "combination", combination, "failed on proc %d:" % proc, a1, a2, b2, "!=", combination
+        return 1
+    else:
+        print "combination", combination, "success"
+        return 0
+
+
+
 def testbed():
+
+    global n_per_proc
 
     testfile_name = "/tmp/test_paramloop.cfg"
 
-    with open(testfile_name, 'w') as testfile:
+    use_mpi = mpi_success and "mpi" in sys.argv[1:]
+    rank = ParameterSetController.get_rank(use_mpi)
 
-        testfile.write("a=[0,1,2]\n")
+    if rank == 0:
+        with open(testfile_name, 'w') as testfile:
+            testfile.write("a=[0,1,2]\n")
+    if use_mpi:
+        comm.Barrier()
 
     any_number = r".*?"
 
@@ -429,11 +475,11 @@ def testbed():
     set2 = ParameterSet(testfile_name, r"a\=\[%s\,(%s)\,%s\]" % (any_number, any_number, any_number))
     set3 = ParameterSet(testfile_name, r"a\=\[%s\,%s\,(%s)\]" % (any_number, any_number, any_number))
 
-    set1.initialize_set_incr( 0,   3,   1)
+    set1.initialize_set([0, 1, 2, 3])
     set2.initialize_set([-2, -0.5, 1])
-    set3.initialize_set_incr( 0, 100,  30, )
+    set3.initialize_set([0, 30, 60, 90])
 
-    controller = ParameterSetController(use_mpi=len(sys.argv) != 1)
+    controller = ParameterSetController(use_mpi=use_mpi)
 
     controller.register_parameter_set(set1)
     controller.register_parameter_set(set2)
@@ -443,7 +489,98 @@ def testbed():
 
     controller.run(exec_test_function, ask=False, n_procs=20, shuffle=True)
 
+    #Double file case
+    #and double params
+
+    testfile2_name = "/tmp/test_paramloop2.cfg"
+
+    if rank == 0:
+        with open(testfile2_name, 'w') as testfile:
+            testfile.write("a=3;\nb=5;")
+    if use_mpi:
+        comm.Barrier()
+
+    set4 = ParameterSet(testfile2_name, [r"a\=(%s)\;" % any_number,
+                                         r"b\=(%s)\;" % any_number])
+    set4.initialize_set([[ 1,  2,  3,  4],
+                         [11, 12, 13, 14]])
+
+    controller2 = ParameterSetController(use_mpi=use_mpi)
+
+    controller2.register_parameter_set(set1)
+    controller2.register_parameter_set(set4)
+
+    controller2.set_repeats(10)
+
+    n_procs = 20
+
+    if use_mpi:
+        n_per_proc = [0 for i in range(comm.size)]
+    else:
+        n_per_proc = [0 for i in range(n_procs)]
+
+    controller2.run(exec_test_function2, ask=False, n_procs=n_procs, shuffle=False)
+
+    if use_mpi:
+        comm.Barrier()
+        time.sleep(comm.rank/100.)
+        print comm.rank, n_per_proc[comm.rank]
+    else:
+        for proc, n in enumerate(n_per_proc):
+            print proc, n
+
+
+def testbed_MPI():
+
+    if comm.rank == 0:
+        my_list = [0,1,2,3,4,5,6,7,8,9,10]
+        n_jobs = len(my_list)
+
+        print "Master start:", sum(my_list)
+
+        #Seeding slaves
+        for i in range(1, comm.size):
+
+            if not my_list:
+                item = None
+            else:
+                item = my_list.pop()
+
+            comm.send(item, dest=i, tag=0)
+
+        #recieving slave result and distributing new jobs
+        s = 0
+        n = 0
+        status = MPI.Status()
+        while n != n_jobs:
+            s += comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+            n += 1
+
+            if my_list:
+                obj = my_list.pop()
+            else:
+                obj = None
+
+            comm.send(obj, dest=status.Get_source(), tag=status.Get_tag())
+
+        print "Master finished:", s
+
+    else:
+        item = comm.recv(source=0, tag=0)
+
+        while item is not None:
+            print item, comm.rank
+
+            comm.send(item, dest=0, tag=comm.rank)
+            item = comm.recv(source=0, tag=comm.rank)
+
+        print "Rank %d finished." % comm.rank
+
+
 if __name__ == "__main__":
-    testbed()
+    if mpi_success and "mpibed" in sys.argv[1:]:
+       testbed_MPI()
+    else:
+        testbed()
 
 
